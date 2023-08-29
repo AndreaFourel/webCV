d46225fd4e11ed5959bc98d825cf6b3907a48c00
Andrea Fourel <andreafourel@hotmail.fr>
2023-08-29 16:17:58 +0200

    add Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className="fixed bottom-0 right-0">
+      <div className="w-0 h-0 border-solid border-[150px] border-t-transparent border-l-transparent border-r-dark-green border-b-dark-green"></div>
+    </footer>
   )
 }
