0b64d31a55717f7472eab05ed58dbd66f01063bd
Andrea Fourel <andreafourel@hotmail.fr>
2023-08-29 16:11:34 +0200

    handle Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="relative flex justify-start">
+      <div className="fixed w-0 h-0 border-solid border-250 border-t-dark-green border-l-dark-green border-r-transparent border-b-transparent">
+        <div className="fixed top-[100px] left-[180px] bg-[url('../public/profile_pic.png')] bg-cover w-[200px] h-[200px] rounded-full bg-[center_top_30%] bg-no-repeat">
+        </div>
+      </div>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        "2xsm": "10px",
+        xsm: "12px",
+        sm: "13px",
+        reg: "15px",
+        lg: "18px",
+        "2xl": "22px",
+        "3xl": "25px",
+        "4xl": "32px",
+        "5xl": "40px",
+        "6xl": "50px",
+        "7xl": "70px",
       },
+      borderWidth: {
+        '250': '250px'
+      },
+      colors: {
+        'dark-green': '#395144',
+        'light-green': '#4E6C50',
+        'dark-sand': '#AA8B56',
+        'light-sand': '#F0EBCE',
+      }
     },
   },
   plugins: [],
